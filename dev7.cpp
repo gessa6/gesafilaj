38d74877feade1464c56f99499a2158132fd0d21
gessa6 <163462540+gessa6@users.noreply.github.com>
2024-03-25 20:53:34 +0100

    Add files via upload

--- dev7.cpp
+++ dev7.cpp
@@ -1,17 +1,42 @@
 #include <stdio.h>
-int main (void){
-    int j, i, n;
-    printf("Vendosni numrin e rreshtave: ");
-    scanf("%d",&n);
-    for( i = i; i <= n; i ++){
-        for( j = 1; j <= n; j ++){
-            if( j<=n - i){
-                printf(" ");
-            }else{
-                printf("*");
-            }
-        }
-        printf("\n");
-    }
-    return 0;
+
+int main() {
+int piket;
+char nota;
+
+printf("Vendosni piket e marra: ");
+scanf("%d",&piket);
+
+if ( piket > 100 ) {
+printf("Piket nuk mund te jene me te larta se 100.\n");
+return 1 ;
 }
+
+switch( piket / 10)
+
+{
+    case 10:
+    case 9:
+       nota='A';
+       break;
+    case 8:
+       nota='B';
+       break;
+    case 7:
+       nota='c';
+       break;
+    case 6:
+       nota='D';
+       break;
+    case 5:
+       nota='F';
+       break;
+    default:
+       nota='F';
+
+}
+printf("Nota juaj eshte:%c\n",nota);
+
+return 0;
+     }
+
